b622d561c9e0bef41017bf3c0b834fba0d3ce062
Yousef Maged <44376674+yousefmaged@users.noreply.github.com>
2026-05-16 20:42:36 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+const CACHE_NAME = 'herp-cache-v1';
+const urlsToCache = ['/app/', '/app/index.html', '/app/styles.css', '/app/db.js', '/app/core.js'];
 
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
